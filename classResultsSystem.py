b7d25575ad512321df1216395497bf7fe029616e
Dnyaneshwar Potdar <dnyaneshwarpotdar2732001@gmail.com>
2026-03-16 15:07:01 +0530

    feat(class-results): add functionality to insert student details including name, roll number, and marks

--- classResultsSystem.py
+++ classResultsSystem.py
@@ -1,6 +1,20 @@
-# UC1 – Initialize Student Records Data Structure
+# UC2 – Add Student Details (Name, Roll Number, Marks)
 
 students = []
 
-print("Student record system initialized.")
-print("Current student records:", students)
+name = input("Enter student name: ")
+roll = int(input("Enter roll number: "))
+marks = float(input("Enter marks: "))
+
+student = {
+    "name": name,
+    "roll": roll,
+    "marks": marks
+}
+
+students.append(student)
+
+print("\nStudent added successfully.")
+print("Current Student Records:")
+for s in students:
+    print(s)
